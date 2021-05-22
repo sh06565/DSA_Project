2a4df3abec79686eb9d42180cc7326b7d6636706
Ibrahim Ali <ibrahim0409@gmail.com>
2021-05-22 13:52:58 +0500

    comments

--- Shopping_Terminal.py
+++ Shopping_Terminal.py
@@ -1,28 +1,39 @@
-# from typing import KeysView
-# import keyboard
+# importig funcitons that will be required to run our code
 from os import system, name
 from time import sleep
 from typing import final
-# check git hub saved? 
+
+
+# using a list to store our inventory, each dictionary inside of the list representing the data for an item. 
+
 def inventory():
     data=  [
-        {"id": 1001, "Info":"1001    Samsung Galaxy A20      Price   "+str(15999),"Name":"Samsung Galaxy A20","Price":15999,"Stock":5},
-        {"id": 1002, "Info":"1002    Samsung Galaxy A21      Price   "+str(18999),"Name":"Samsung Galaxy A21","Price":18999,"Stock":5},
-        {"id": 1003, "Info":"1003    Samsung Galaxy A30      Price   "+str(23999),"Name":"Samsung Galaxy A30","Price":23999,"Stock":5},
-        {"id": 1004, "Info":"1004    Samsung Galaxy A51      Price   "+str(41999),"Name":"Samsung Galaxy A51","Price":41999,"Stock":5},
-        {"id": 1005, "Info":"1005    Samsung Galaxy A52      Price   "+str(44999),"Name":"Samsung Galaxy A52","Price":44999,"Stock":5},
-        {"id": 1006, "Info":"1006    Samsung Galaxy A61      Price   "+str(53499),"Name":"Samsung Galaxy A61","Price":53499,"Stock":5},
-        {"id": 1007, "Info":"1007    Samsung Galaxy A72      Price   "+str(59999),"Name":"Samsung Galaxy A72","Price":59999,"Stock":5},
-        {"id": 1008, "Info":"1008    Samsung Galaxy A81      Price   "+str(61999),"Name":"Samsung Galaxy A81","Price":61999,"Stock":5},
-        {"id": 1009, "Info":"1009    Samsung Galaxy A82      Price   "+str(67999),"Name":"Samsung Galaxy A82","Price":67999,"Stock":5}
-        ]
+        {"id": 1001, "Info":"1001    Chocolate Cupcake         Price   "+str(120),"Name":"Chocolate Cupcake","Price":120,"Stock":10},
+        {"id": 1002, "Info":"1002    Butterscotch Cupcake      Price   "+str(120),"Name":"Butterscotch Cupcake","Price":120,"Stock":10},
+        {"id": 1003, "Info":"1003    Red Velvet Cupcake        Price   "+str(120),"Name":"Red Velvet Cupcake","Price":120,"Stock":10},
+        {"id": 1004, "Info":"1004    Hershey's Brownie         Price   "+str(80),"Name":"Hershey's Brownie","Price":80,"Stock":10},
+        {"id": 1005, "Info":"1005    Fudge Brownie             Price   "+str(100),"Name":"Fudge Brownie","Price":100,"Stock":10},
+        {"id": 1006, "Info":"1006    Oreo Shake                Price   "+str(499),"Name":"Oreo Shake","Price":499,"Stock":10},
+        {"id": 1007, "Info":"1007    Vanilla Almond Milk       Price   "+str(499),"Name":"Vanilla Almond Milk","Price":499,"Stock":10},
+        {"id": 1008, "Info":"1008    Raspberry Cupcake         Price   "+str(120),"Name":"Raspberry Cupcake","Price":120,"Stock":10},
+        {"id": 1009, "Info":"1009    Lotus Brownie             Price   "+str(100),"Name":"Lotus Brownie","Price":100,"Stock":10}
+    ]
     return data
+
+# displays the inventory to the user
+
 def userMenu(x):
     for each in x:
         print(each["Info"])
+
+# clears the screen
+
 def clear():
     if name == 'nt':
         _ = system('cls')
+
+# sorts the given field
+  
 def bubble_sort(lst,check2):
     x=len(lst)
     if x>1:
@@ -38,6 +49,9 @@ def bubble_sort(lst,check2):
                     if lst[i]["Price"]<lst[i+1]["Price"]:
                         lst[i],lst[i+1]=lst[i+1],lst[i]
             return lst
+
+# searches for the given item in the list
+
 def binary_search_iterative(lst, item):
     low,high=0, len(lst)-1
     while low<=high:
@@ -49,6 +63,9 @@ def binary_search_iterative(lst, item):
         elif item<lst[mid]["id"]:
             high=mid-1
     return -1
+
+# gives us the paths for our required list
+
 def dijsktra(graph,start,end):
     lst_nodes=list(graph.keys())
     short_dist,visited,infinity={},[],9999
@@ -90,61 +107,101 @@ def dijsktra(graph,start,end):
     print()
     print("==> The time taken to reach your area will be  "+str(time)+" mins.")
     print()
+
+# filters the inventory data, by either high or low using bubble sort
+
 def filter():
+    # print() statement for spacing
     print()
-    check=input("Would you like to filter the prices? Y or N? ")
+    # asks user if they would like to filter the prices
+    check=input("Would you like to filter the prices? Y or N? ") 
     if check.lower()=="y":
         print()
+        # if yes, then we ask if they would like to filter from low to high or high to low
         check2=input("To filter from low to high Enter L, Else H. ")
         if check2=="l":
             clear()
+            # using bubble sort we will sort our inventory from low to high 
             x=bubble_sort(inventory(),check2)
+            # display inventory 
             print("Prices filtered from low to high")
             print("ID      Model                   Price   PKR")
             userMenu(x)
         elif check2.lower()=="h":
             clear()
+            # using bubble sort we will sort our inventory from low to high 
             x=bubble_sort(inventory(),check2)
+            # display inventory 
             print("Prices filtered from high to low")
             print("ID      Model                   Price   PKR")
             userMenu(x)
     elif check.lower()=="n":
+        # if they do not want to filter we pass
         pass
+
+# updates the stock in the inventory when changes are made to cart
+
 def stock(qnty,position):
+    # gets current stock of item from inventory 
     x=(inventory()[position])["Stock"]
+    # compares the stock with quantity entered by the user  
+    # if the quantity equals to stock or if it is lower than stock then stock is updated in iventory and returns the updated stock value
     if x==qnty or x>qnty:
         y=inventory()
         d=y[position]
         d["Stock"]=x-qnty
         return x 
+    # else if stock is less than our quantity then negative stock is returned
     elif x<qnty:
         return -x
+
+# adds selected items to cart 
+
 def add(cart):
+    # asks user for the item that they would like to add to cart
     item=int(input("Enter the id of the item u would like to add: "))
+    # we get the index of our dictionary from the list 
     position=binary_search_iterative(inventory(),item)
+    # check for if index in list 
     if position!=-1:
+        # ask for the required quantity of the item
         qnty=int(input("Kindly enter the quantity of the item: "))
+        # check, using our stock fucniton, that if the entered quantity is available 
         ok=stock(qnty,position)
         if ok<0:
+            # if there isnt enough stock it displays an error msg and calls the add(cart) fucntion back
             print("There isn't that much stock, only "+str(-1*ok)+" pieces are available.")
             print("Kindly selecct the quantity accordingly!")
             add(cart)
         else:    
+            # if stock is present then it adds the to the cart with the set quantity and their total price
             price=(inventory()[position])["Price"]*qnty
             name=(inventory()[position])["Name"]
             cart[(inventory()[position])["id"]]=[name,qnty,price]
             print("Item added to cart!")
     else:
+        # if id not in inventory then it outputs an error msg
         print("Enter a valid id to add!")
+
+# deletes the selected item from the cart 
+
 def remove(cart):
+    # displays cart to user 
     viewcart(cart)
-    item=int(input("Enter the item that you would like to remove: "))
+    # inputs for the item id to be removed  
+    item=int(input("Enter the item id that you would like to remove: "))
+    # checks if item is present in cart
     if item in cart.keys():
+        # inputs the quantity of that item to be removed
         qnty=int(input("Also enter the quantity that you would like to remove: "))
+        # if the cart quantity matches the entered quantity 
         if cart[item][1]==qnty or cart[item][1]==1:
+            # deletes that item from cart  
             del(cart[item])
+        # else if quantitiy does not match the items quantity in cart then it tells the user to enter a valid quantity  
         elif cart[item][1]<qnty:
             print("Entered quantity doesn't exist, enter a valid quantity to remove!")
+        # else, then it removes the set quantity and updates the 
         else:
             intial=cart[item][1]
             cart[item][1]=(cart[item][1])-qnty
